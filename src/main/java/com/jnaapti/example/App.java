ccb5c9f9087b3823b68956a3e2fbd4cf22f6b25b
Gautham Pai <buzypi@gmail.com>
2022-08-05 16:50:35 +0530

    Update App.java

--- src/main/java/com/jnaapti/example/App.java
+++ src/main/java/com/jnaapti/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello, committing from Github!" );
+        System.out.println( "Hello, committing from Github - trying again!" );
     }
 }
